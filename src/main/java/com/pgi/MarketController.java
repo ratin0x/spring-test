1c1f26203096371818154e9c87bb333bd75e0f76
Paul Fox <paul.fox@pgitl.com>
2016-02-11 15:44:09 +0000

    Added endpoint

--- src/main/java/com/pgi/MarketController.java
+++ src/main/java/com/pgi/MarketController.java
@@ -1,8 +1,22 @@
 package com.pgi;
 
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MarketController {
+	
+	@RequestMapping("/prodlist")
+	public Map<String, Object> list() {
+		Map<String, Object> prodList = new HashMap<String, Object>();
+		
+		prodList.put( "1", "{ 'prodName': 'ECA', 'prodCode': '123456', 'prodCost': '£10000'}");
+		prodList.put( "2", "{ 'prodName': 'CISSP', 'prodCode': '789012', 'prodCost': '£20000'}");
+		
+		return prodList;
+	}
 
 }
